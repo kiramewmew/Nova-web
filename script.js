68262c6771a3a3a6d1ede2b80259db4c613ca37f
kiramewmew <bakufan94@gmail.com>
2026-06-02 07:44:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ btnReportar.addEventListener('click', () => {
 btnManual.addEventListener('click', () => {
     alert("Abriendo el manual de usuario. Prepárate para 50 páginas de texto aburrido ;)");
 });
+// 1. Seleccionamos el botón y el menú nav
+const botonMenu = document.querySelector('#boton-menu');
+const menuNavegacion = document.querySelector('#navegacion');
+
+// 2. Escuchamos el clic en el botón hamburguesa
+botonMenu.addEventListener('click', () => {
+    // 3. El toggle quita o pone la clase "menu-oculto" dinámicamente
+    menuNavegacion.classList.toggle('menu-oculto');
+});
